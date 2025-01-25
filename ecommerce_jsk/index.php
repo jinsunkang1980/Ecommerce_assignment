cf590b3c9d61e4fa70b506b762a337b8794cf3ab
jinsunkang1980 <jinsunkang1980@gmail.com>
2025-01-24 21:51:20 -0500

    Update index.php

--- ecommerce_jsk/index.php
+++ ecommerce_jsk/index.php
@@ -1,4 +1,4 @@
 <?php include 'includes/header.php'; ?>
-<h1>Welcome to Our E-commerce Website</h1>
-<p>Shop the latest products at affordable prices!</p>
+<h1>Welcome to Theos Canada</h1>
+<p>Shop the latest clothes at affordable prices!</p>
 <?php include 'includes/footer.php'; ?>
