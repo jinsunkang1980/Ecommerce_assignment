ece8e69aea6fbae04e4c137d4c0506dfcd648e9b
jinsunkang1980 <jinsunkang1980@gmail.com>
2025-01-29 00:13:05 -0500

    Update logout.php

--- ecommerce_jsk/logout.php
+++ ecommerce_jsk/logout.php
@@ -1,9 +1,12 @@
-
 <?php
-// Clear the user_id cookie
-setcookie('user_id', '', time() - 3600, '/'); // Expire the cookie by setting a past time
-setcookie('user_name', '', time() - 3600, '/'); // Expire the cookie by setting a past time
+// Start session (optional)
+session_start();
 
-header('Location: index.php'); // Redirect to index/home page
-exit;
+// Unset all cookies by setting their expiration time to the past
+setcookie('user_id', '', time() - 3600, '/');
+setcookie('user_name', '', time() - 3600, '/');
+
+// Redirect user to login page
+header("Location: login.php");
+exit();
 ?>
