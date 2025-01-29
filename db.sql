8f834e9ea36dd669f2b75485d90799287a73f13a
jinsunkang1980 <jinsunkang1980@gmail.com>
2025-01-28 20:25:27 -0500

    Update db.sql

--- db.sql
+++ db.sql
@@ -49,11 +49,11 @@ CREATE TABLE order_items (
 );
 
 INSERT INTO ccbst.products (name, price, description, image) VALUES
-('Product A', 10.00, 'Description for Product A.', 'uploads/a.jpeg'),
-('Product B', 15.50, 'Description for Product B.', 'uploads/b.jpeg'),
-('Product C', 20.99, 'Description for Product C.', 'uploads/c.jpeg'),
-('Product D', 25.00, 'Description for Product D.', 'uploads/d.jpeg'),
-('Product E', 30.00, 'Description for Product E.', 'uploads/e.jpeg');
+('Black Mesh Monika Dress', 180.00, 'With a panelled corset-style top, that cinches your waist to create a beautifully structured outline, which then drapes, from the scalloped edging, into a gorgeous pleated tulle mesh skirt.', 'uploads/cloth1.jpeg'),
+('Artemis Slip Skirt', 210, 'The Artemis Beading Slip Skirt, crafted from delicate Japanese nylon tulle, is the seasons ultimate Have to Have..', 'uploads/cloth2.jpeg'),
+('Dominic Pinstripe Liam Vest', 190, 'This assortment serves as your WOFs (Without Fails) and are designed to create space in your wardrobe for creativity and experimentation..', 'uploads/cloth3.jpeg'),
+('Dominic Pinstripe Reese Sculpted Trouser', 325.00, 'This assortment serves as your WOFs (Without Fails) and are designed to create space in your wardrobe for creativity and experimentation..', 'uploads/cloth4.jpeg'),
+('Crispy Nylon Maxi Skirt', 230.00, 'This assortment serves as your WOFs (Without Fails) and are designed to create space in your wardrobe for creativity and experimentation.', 'uploads/cloth5.jpeg');
 
 -- INSERT INTO ccbst.users (name, email, password) VALUES
 -- ('admin', 'admin@admin.com', 'admin');
