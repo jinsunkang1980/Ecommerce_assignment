d78a6df7200656cf058d325d799dced78f2afca6
jinsunkang1980 <jinsunkang1980@gmail.com>
2025-01-29 00:09:41 -0500

    Update footer.php

--- ecommerce_jsk/includes/footer.php
+++ ecommerce_jsk/includes/footer.php
@@ -1,5 +1,29 @@
 <footer>
-        <p>&copy; 2025 Theos Canada by Jinsun Kang. All rights reserved.</p>
-    </footer>
-</body>
-</html>
+    <p>&copy; 2025 Theos Canada by Jinsun Kang. All rights reserved.</p>
+</footer>
+
+<style>
+    /* Make sure the body takes the full height */
+    html, body {
+        height: 100%;
+        margin: 0;
+        padding: 0;
+        display: flex;
+        flex-direction: column;
+    }
+
+    /* Push content to take available space */
+    .content {
+        flex: 1;
+    }
+
+    /* Fixed Footer at the Bottom */
+    footer {
+        text-align: center;
+        padding: 15px;
+        width: 100%;
+        background-color: #f8f8f8;
+        position: relative;
+        bottom: 0;
+    }
+</style>
